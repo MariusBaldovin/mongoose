04916a9a401fc7211dfb14f30ffd788839ba9217
MariusBaldovin <youremailid@example.com>
2024-02-13 22:31:14 +0000

    connected mongo db database

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB Atlas!"))
   .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
 
-let Person;
+let Person1;
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true },
