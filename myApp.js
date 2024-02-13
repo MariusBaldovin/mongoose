1bbcd6660fca6f3edc8e7514f53013fb70492f1c
MariusBaldovin <youremailid@example.com>
2024-02-13 21:27:25 +0000

    connected mongo db database

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas!"))
+  .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
 
 let Person;
 
